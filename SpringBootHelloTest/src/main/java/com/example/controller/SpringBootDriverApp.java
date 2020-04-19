3195b218356937f0401a1fb6f7be7340754c2301
“Sudhir <sudgaura@cisco.com>
2020-04-19 21:35:36 +0530

    Fixing issue In mail

--- SpringBootHelloTest/src/main/java/com/example/controller/SpringBootDriverApp.java
+++ SpringBootHelloTest/src/main/java/com/example/controller/SpringBootDriverApp.java
@@ -1,9 +1,15 @@
 package com.example.controller;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringBootDriverApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("starting spring Boot app");
+		SpringApplication.run(SpringBootDriverApp.class, args);
 
 	}
 
