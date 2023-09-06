c3d1952bdba31338e5baedd7275fd2bcf6264bc1
sudgaura <sudgaura@cisco.com>
2023-09-06 19:16:45 +0530

    PDF reader project

---
+++ pdfreader/src/main/java/com/example/pdfreader/pdfreader/PdfreaderApplication.java
@@ -0,0 +1,14 @@
+package com.example.pdfreader.pdfreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfreaderApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(PdfreaderApplication.class, args);
+		System.out.println("Hiiii");
+	}
+
+}
