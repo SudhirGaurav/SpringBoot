15c15d40131e209e10f512cfe2d1135005e04f4c
sudhirgaurav <sudhirgauravec@gmail.com>
2024-03-31 10:08:04 +0530

    second checkin

---
+++ data_operation_service/src/main/java/com/example/data_operation_service/data_operation_service/DataOperationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_operation_service.data_operation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataOperationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataOperationServiceApplication.class, args);
+	}
+
+}
---
+++ data_operation_service/src/test/java/com/example/data_operation_service/data_operation_service/DataOperationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data_operation_service.data_operation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataOperationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
