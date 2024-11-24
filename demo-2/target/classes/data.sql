7afee8afb5967788ac647bda8283a6754f773261
SudhirGaurav <41154292+SudhirGaurav@users.noreply.github.com>
2024-11-24 19:00:16 +0530

    Add files via upload
    Sprin boot cache

---
+++ demo-2/target/classes/data.sql
@@ -0,0 +1,20 @@
+insert into DEVICE_INTERFACE
+(id,name,DEVICE_NAME)
+values(10001,'Optics','2k');
+
+insert into DEVICE_INTERFACE
+(id,name,DEVICE_NAME)
+values(10002,'packet','ASR');
+
+insert into DEVICE_INTERFACE
+(id,name,DEVICE_NAME)
+values(10003,'coherent','2k');
+
+insert into DEVICE_INTERFACE
+(id,name,DEVICE_NAME)
+values(10004,'Optics','2k');
+
+insert into DEVICE_INTERFACE
+(id,name,DEVICE_NAME)
+values(10005,'Optics','2k');
+
